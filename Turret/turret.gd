e7874757810064d550b0b48592de02d08c1f013c
ozgencdev <79049138+ozgencDev@users.noreply.github.com>
2025-02-06 23:50:20 +0300

    with turret manager we can pass our  path  to turret, and with that path we can reach their children(enemy) then we use  look at func  with 3 param also we can set direction depends on global basis(instead of local basis) securing for turret look at func

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -2,7 +2,14 @@ extends Node3D
 @export var projectile: PackedScene 
 @onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
 
+var path: Path3D 
+
+func _physics_process(delta: float) -> void:
+	var enemy = path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
+
 func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
 	shot.global_position = turret_top.global_position
+	shot.direction = global_transform.basis.z
