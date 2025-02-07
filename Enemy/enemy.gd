2a4275c46b5639a50f1e941507b6151c4cd9a667
ozgencdev <79049138+ozgencDev@users.noreply.github.com>
2025-02-07 23:30:23 +0300

    creates health system for enemy,  adds area3d  and  col shape, adds signal for projectile  when on entered the are  it reaches that  area and call its function for applying damage then removes themselves, each turret finds best target for  themselves depends  range and enemy progress

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,12 +1,30 @@
 extends PathFollow3D
 
 
-@export var speed: float = 2.5
+@export var speed: float = 1
+@export var max_health := 50
 @onready var base = get_tree().get_first_node_in_group("base")
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		print(current_health)
+		if(current_health < 1):
+			queue_free()
+		
+
+func _ready() -> void:
+	current_health = max_health
+	
+
+func take_damage() -> void:
+	current_health -= 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += delta * speed
+	print(progress)
 	if progress_ratio == 1.0:
 		base.take_damage()
 		set_process(false)
+
--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,15 +1,36 @@
 extends Node3D
 @export var projectile: PackedScene 
 @onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+@export var turret_range := 10
+
+
+var enemy: PathFollow3D
+
 
 var path: Path3D 
 
 func _physics_process(delta: float) -> void:
-	var enemy = path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	enemy = find_best_target()
+	if enemy:
+		look_at(enemy.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
-	var shot = projectile.instantiate()
-	add_child(shot)
-	shot.global_position = turret_top.global_position
-	shot.direction = global_transform.basis.z
+	if enemy:
+		var shot = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = turret_top.global_position
+		shot.direction = global_transform.basis.z
+
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	
+	for enemy in path.get_children():
+		if enemy is PathFollow3D:
+			if global_position.distance_to(enemy.global_position) < turret_range:
+				if enemy.progress > best_progress:
+					best_target = enemy
+					best_progress = enemy.progress
+	
+	return best_target
