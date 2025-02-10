088fdc1c05bc98a098cc0d5b00bda34105be05cc
ozgencdev <79049138+ozgencDev@users.noreply.github.com>
2025-02-10 23:58:49 +0300

    adds signal to difficulty_manager for stop spawning enemy and binds with timer timeout signal that signal then we catch that signal from Enemypatalso health curve

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -4,6 +4,7 @@ extends PathFollow3D
 @export var speed: float = 1
 @export var max_health := 50
 @export var coin := 15
+
 @onready var base = get_tree().get_first_node_in_group("base")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var bank = get_tree().get_first_node_in_group("bank")
@@ -21,6 +22,7 @@ var current_health: int:
 
 func _ready() -> void:
 	current_health = max_health
+	#Engine.time_scale = 5 We can speed engine time for testing purposes
 	
 
 func take_damage() -> void:
@@ -33,4 +35,5 @@ func _process(delta: float) -> void:
 	if progress_ratio == 1.0:
 		base.take_damage()
 		set_process(false)
+		queue_free()
 
--- EnemyPath.gd
+++ EnemyPath.gd
@@ -8,3 +8,7 @@ func spawn_enemy() -> void:
 	var new_enemy = enemy_scene.instantiate()
 	add_child(new_enemy)
 	timer.wait_time = difficulty_manager.get_spawn_time()
+
+
+func _on_difficulty_manager_stop_spawning_enemies() -> void:
+	timer.stop()
